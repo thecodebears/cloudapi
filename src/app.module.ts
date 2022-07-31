d7bdf1c547fe7aa2c1e85b1eca9dbddeb4d4a48c
anore <66754841+codelumen@users.noreply.github.com>
2022-07-31 16:30:37 +0300

    Fixed indentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnoroModule } from './enoro/enoro.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [EnoroModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
