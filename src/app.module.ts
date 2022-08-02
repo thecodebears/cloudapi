ea3a827ac5ffdb3c2de8163f57393581c5a6bc96
anore <66754841+codelumen@users.noreply.github.com>
2022-08-02 08:52:24 +0300

    Fixed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
 import { EnoroModule } from './enoro/enoro.module';
-import { AccountModule } from './models/account/account.model';
+import { Account } from './models/account.entity';
+import { AccountModule } from './account/account.module';
 
 @Module({
     imports: [
