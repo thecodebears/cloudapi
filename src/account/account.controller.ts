8f14252200801997f5ecae9151e4189d47a79dd2
anore <66754841+codelumen@users.noreply.github.com>
2022-08-02 10:08:25 +0300

    Revert test changes

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -2,20 +2,4 @@ import { Controller, Get, Post, Query } from "@nestjs/common";
 import { AccountService } from "./account.service";
 
 @Controller('account')
-export class AccountController {
-    constructor(private accountService: AccountService) {}
-
-    @Post('create')
-    public async create(@Query('name') name: string) {
-        let account = await this.accountService.create(name);
-
-        return account;
-    }
-
-    @Get()
-    public async get(@Query('name') id: string) {
-        let account = await this.accountService.findById(id);
-
-        return account;
-    }
-}
+export class AccountController {}
