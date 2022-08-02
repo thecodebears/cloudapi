eba6a8b67606627f35a2bfecd0c4aa3fd809fdd7
anore <66754841+codelumen@users.noreply.github.com>
2022-08-02 04:17:20 +0300

    Initialized PostgreSQL base entity

---
+++ src/models/base.entity.ts
@@ -0,0 +1,27 @@
+import { PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn } from 'typeorm';
+
+export abstract class BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    public id: string;
+
+    @Column({ type: 'boolean', default: true })
+    public isActive: boolean;
+
+    @Column({ type: 'boolean', default: false })
+    public isArchived: boolean;
+
+    @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    public createDateTime: Date;
+
+    @Column({ type: 'varchar', length: 300 })
+    public createdBy: string;
+
+    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    public lastChangedDateTime: Date;
+
+    @Column({ type: 'varchar', length: 300 })
+    public lastChangedBy: string;
+
+    @Column({ type: 'varchar', length: 300, nullable: true })
+    public internalComment: string | null;
+}
