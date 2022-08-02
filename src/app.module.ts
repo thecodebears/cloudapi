d34b391958390e10e947c9d1219f95dc4aa12cd8
anore <66754841+codelumen@users.noreply.github.com>
2022-08-02 18:33:04 +0300

    Updated module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnoroModule } from './enoro/enoro.module';
-import { Account } from './models/account.entity';
-import { AccountModule } from './account/account.module';
+import { ClientModule } from './models/client/client.module';
+import { AuthModule } from './auth/auth.module';
+import { ApplicationModule } from './models/application/application.module';
+import { UserModule } from './models/user/user.module';
 
 @Module({
     imports: [
-        EnoroModule,
-        AccountModule,
-        TypeOrmModule.forRoot(ormConfig)
+        ClientModule,
+        AuthModule,
+        ApplicationModule,
+        ClientModule,
+        UserModule,
+        TypeOrmModule.forRoot(ormConfig),
     ],
     controllers: [ AppController ],
     providers: [ AppService ],
