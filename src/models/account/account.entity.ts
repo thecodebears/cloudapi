a475f091d8d3a79b0658d600b9e381b8bfde6594
anore <66754841+codelumen@users.noreply.github.com>
2022-08-02 07:27:30 +0300

    Fixed columns types

--- src/models/account/account.entity.ts
+++ src/models/account/account.entity.ts
@@ -3,10 +3,10 @@ import { BaseEntity } from "../base.entity";
 
 @Entity()
 export class Account extends BaseEntity {
-    @Column({ type: 'string' })
+    @Column({ type: 'varchar' })
     public token: string;
 
-    @Column({ type: 'string' })
+    @Column({ type: 'varchar' })
     public name: string;
 
     @Column({ type: 'boolean' })
