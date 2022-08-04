c099f42f627dd85c0226b8a4f159fa6023e8ebaa
anore <66754841+codelumen@users.noreply.github.com>
2022-08-04 06:56:59 +0300

    Removed deprecated models imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/models/user/user.module';
-import { ClientModule } from 'src/models/client/client.module';
 
 @Module({
     imports: [
-        UserModule,
-        ClientModule
+        UserModule
     ],
     controllers: [ AuthController ],
     providers: [ AuthService ]
