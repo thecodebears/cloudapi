34f6c63e02f34bf79323fd0bb93d39c39c647efb
anore <66754841+codelumen@users.noreply.github.com>
2022-08-04 06:58:28 +0300

    Codes migrated to user.types

--- src/models/user/user.types.ts
+++ src/models/user/user.types.ts
@@ -5,3 +5,30 @@ export type UsersFindQuery = {
     id?: string,
     username?: string
 };
+
+/**
+ * Response codes.
+ */
+export enum UserResponse {
+    /**
+     * Success responses.
+     */
+    Valid='Valid',
+    Updated='Updated',
+    Destroyed='Destroyed',
+    ClientLoggedIn='LoggedIn',
+    ClientLoggedOut='LoggedOut',
+    ApplicationProvided='ApplicationProvided',
+    ApplicationRemoved='ApplicationProvided',
+
+    /**
+     * Error responses.
+     */
+    NotFound='NotFound',
+    NotValid='NotValid',
+    UsernameWasTaken='UsernameWasTaken',
+    ClientAlreadyLoggedIn='ClientAlreadyLoggedIn',
+    ClientNotLoggedIn='ClientNotLoggedIn',
+    ApplicationAlreadyProvided='ApplicationAlreadyProvided',
+    ApplicationNotProvided='ApplicationNotProvided'
+}
