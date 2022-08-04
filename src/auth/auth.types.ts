94d2aad86f5317054890e5b19d2a87edf1fddf86
anore <66754841+codelumen@users.noreply.github.com>
2022-08-04 06:56:41 +0300

    Initialized Auth types

---
+++ src/auth/auth.types.ts
@@ -0,0 +1,14 @@
+/**
+ * Response codes.
+ */
+export enum AuthResponse {
+    /**
+     * Success responses.
+     */
+    AuthSessionClosed='AuthSessionClosed',
+
+    /**
+     * Error responses.
+     */
+    AuthSessionNotExists='AuthSessionNotExists'
+}
