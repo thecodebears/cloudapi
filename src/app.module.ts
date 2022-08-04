5d548b29f61538775d7acf56e9cfcbe9dbe3d625
anore <66754841+codelumen@users.noreply.github.com>
2022-08-04 06:57:29 +0300

    Removed deprecated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientModule } from './models/client/client.module';
 import { AuthModule } from './auth/auth.module';
-import { ApplicationModule } from './models/application/application.module';
 import { UserModule } from './models/user/user.module';
 
 @Module({
     imports: [
-        ClientModule,
         AuthModule,
-        ApplicationModule,
-        ClientModule,
         UserModule,
         TypeOrmModule.forRoot(ormConfig),
     ],
