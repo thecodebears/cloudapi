46c922be4a0feb799e497c2aec90492cbe608580
anore <66754841+codelumen@users.noreply.github.com>
2022-08-09 15:44:41 +0300

    Wiped user types

--- src/models/user/user.types.ts
+++ src/models/user/user.types.ts
@@ -1,34 +1,14 @@
 /**
- * Users finding parameters
- */
-export type UsersFindQuery = {
-    id?: string,
-    username?: string
-};
-
-/**
  * Response codes.
  */
 export enum UserResponse {
     /**
      * Success responses.
      */
-    Valid='Valid',
-    Updated='Updated',
-    Destroyed='Destroyed',
-    ClientLoggedIn='LoggedIn',
-    ClientLoggedOut='LoggedOut',
-    ApplicationProvided='ApplicationProvided',
-    ApplicationRemoved='ApplicationProvided',
+
 
     /**
      * Error responses.
      */
-    NotFound='NotFound',
-    NotValid='NotValid',
-    UsernameWasTaken='UsernameWasTaken',
-    ClientAlreadyLoggedIn='ClientAlreadyLoggedIn',
-    ClientNotLoggedIn='ClientNotLoggedIn',
-    ApplicationAlreadyProvided='ApplicationAlreadyProvided',
-    ApplicationNotProvided='ApplicationNotProvided'
+
 }
