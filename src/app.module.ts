c14e05fe56b15e4e2c12ddb49a26d075f328620d
anore <66754841+codelumen@users.noreply.github.com>
2022-08-09 15:47:45 +0300

    Added ApplicationModule, UtilsModule and ConfigModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
+import { applicationConfig } from './../config/application.config';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './models/user/user.module';
+import { integrationsConfig } from 'config/integrations.config';
+import { ApplicationModule } from './models/application/application.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
     imports: [
         AuthModule,
         UserModule,
+        ApplicationModule,
+        UtilsModule,
         TypeOrmModule.forRoot(ormConfig),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [ applicationConfig, integrationsConfig ]
+        })
     ],
     controllers: [ AppController ],
     providers: [ AppService ],
