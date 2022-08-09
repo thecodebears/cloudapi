1dda7ea5f497e2aac833b0ad3446a1ad4295dd07
anore <66754841+codelumen@users.noreply.github.com>
2022-08-09 15:45:57 +0300

    More auth responses

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -6,9 +6,21 @@ export enum AuthResponse {
      * Success responses.
      */
     AuthSessionClosed='AuthSessionClosed',
+    Registered='Registered',
+    LoggedOut='LoggedOut',
 
     /**
      * Error responses.
      */
-    AuthSessionNotExists='AuthSessionNotExists'
+    InvalidArguments='InvalidArguments',
+    AuthSessionNotExists='AuthSessionNotExists',
+    UsernameWasTaken='UsernameWasTaken',
+    UserNotFound='UserNotFound',
+    InvalidPassword='InvalidPassword',
+    AlreadyLoggedIn='AlreadyLoggedIn'
 }
+
+export type UserAuthorizationData = {
+    username?: string,
+    token?: string
+};
